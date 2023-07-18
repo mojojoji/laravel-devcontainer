a7b8aaafb21d341ddd1eecef5f29b35e1c399958
Joji Augustine <jojimail@gmail.com>
2023-07-18 20:53:56 +0000

    devcontainers

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -349,8 +349,8 @@ class ComposerStaticInitf7b63826f202bbfc003f9abd8a852ceb
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
